cde834a41f1e7d57c4d7c0d6f6e54dfce5e6ba62
Nipun Dineshan <nipundineshan@gmail.com>
2026-05-17 23:00:14 +0400

    updated

--- src/app/core/services/plot.service.ts
+++ src/app/core/services/plot.service.ts
@@ -6,7 +6,7 @@ import { environment } from '../../../environments/environment';
 import { AppStateService } from './app-state.service';
 
 @Injectable({
-  providedIn: 'root'
+  providedIn: 'root',
 })
 export class PlotService {
   private http = inject(HttpClient);
@@ -16,70 +16,70 @@ export class PlotService {
   getAllPlots(): Observable<Plot[]> {
     this.appState.setLoading(true);
     return this.http.get<any>(this.apiUrl).pipe(
-      map(res => res.data || res),
-      tap(() => this.appState.setLoading(false))
+      map((res) => res.data || res),
+      tap(() => this.appState.setLoading(false)),
     );
   }
 
   getMyPlots(): Observable<Plot[]> {
     this.appState.setLoading(true);
     return this.http.get<any>(`${this.apiUrl}/my-plots`).pipe(
-      map(res => res.data || res),
-      tap(() => this.appState.setLoading(false))
+      map((res) => res.data || res),
+      tap(() => this.appState.setLoading(false)),
     );
   }
 
   getPlotById(id: string): Observable<Plot> {
     this.appState.setLoading(true);
     return this.http.get<any>(`${this.apiUrl}/${id}`).pipe(
-      map(res => res.data || res),
-      tap(() => this.appState.setLoading(false))
+      map((res) => res.data || res),
+      tap(() => this.appState.setLoading(false)),
     );
   }
 
   createPlot(formData: FormData): Observable<Plot> {
     this.appState.setLoading(true);
     return this.http.post<any>(this.apiUrl, formData).pipe(
-      map(res => res.data || res),
-      tap(() => this.appState.setLoading(false))
+      map((res) => res.data || res),
+      tap(() => this.appState.setLoading(false)),
     );
   }
 
   updatePlot(id: string, formData: FormData): Observable<Plot> {
     this.appState.setLoading(true);
     return this.http.patch<any>(`${this.apiUrl}/${id}`, formData).pipe(
-      map(res => res.data || res),
-      tap(() => this.appState.setLoading(false))
+      map((res) => res.data || res),
+      tap(() => this.appState.setLoading(false)),
     );
   }
 
   approvePlot(id: string): Observable<Plot> {
-    return this.http.patch<any>(`${this.apiUrl}/${id}/approve`, {}).pipe(
-      map(res => res.data || res)
-    );
+    return this.http
+      .put<any>(`${this.apiUrl}/${id}`, {})
+      .pipe(map((res) => res.data || res));
   }
 
   rejectPlot(id: string, reason: string): Observable<Plot> {
-    return this.http.patch<any>(`${this.apiUrl}/${id}/reject`, { reason }).pipe(
-      map(res => res.data || res)
-    );
+    return this.http
+      .patch<any>(`${this.apiUrl}/${id}/reject`, { reason })
+      .pipe(map((res) => res.data || res));
   }
 
   uploadToIpfs(id: string): Observable<{ ipfsHash: string }> {
-    return this.http.post<any>(`${this.apiUrl}/${id}/ipfs`, {}).pipe(
-      map(res => res.data || res)
-    );
+    return this.http
+      .post<any>(`${this.apiUrl}/${id}/ipfs`, {})
+      .pipe(map((res) => res.data || res));
   }
 
   getStats(): Observable<any> {
-    return this.http.get<any>(`${this.apiUrl}/stats`).pipe(
-      map(res => res.data || res)
-    );
+    return this.http
+      .get<any>(`${this.apiUrl}/stats`)
+      .pipe(map((res) => res.data || res));
   }
 
   getGlobalStats(): Observable<any> {
-    return this.http.get<any>(`${this.apiUrl}/global-stats`).pipe(
-      map(res => res.data || res)
-    );
+    return this.http
+      .get<any>(`${this.apiUrl}/global-stats`)
+      .pipe(map((res) => res.data || res));
   }
 }
